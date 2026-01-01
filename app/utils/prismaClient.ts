733e4a9d7e1c22cf25e06a969acc498b027dfffa
Abhinav Singh <asingh308@nait.ca>
2025-12-31 17:29:53 -0700

    Update prismaClient.ts

--- app/utils/prismaClient.ts
+++ app/utils/prismaClient.ts
@@ -2,6 +2,6 @@ import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URl });
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 export const prisma = new PrismaClient({ adapter });
